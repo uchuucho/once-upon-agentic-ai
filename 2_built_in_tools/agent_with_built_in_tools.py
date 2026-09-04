061c83692fcc9d15741e3bdcc6c1b8b95e20232f
Asma <136265381+uchuucho@users.noreply.github.com>
2026-09-04 20:45:28 +0200

    Q2 Done

--- 2_built_in_tools/agent_with_built_in_tools.py
+++ 2_built_in_tools/agent_with_built_in_tools.py
@@ -1,9 +1,11 @@
 from strands import Agent
 # TODO: Import the current_time built-in tool
+from strands_tools import current_time
 
 agent = Agent(
     tools=[
         # TODO: Add the current_time tool to your agent
+        current_time
     ]
 )
 
