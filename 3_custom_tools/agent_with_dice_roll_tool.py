c1ccd48cbeb0fa741b7e98ef911c6ea1e968455e
Asma <136265381+uchuucho@users.noreply.github.com>
2026-09-04 20:46:17 +0200

    Q3 Done

--- 3_custom_tools/agent_with_dice_roll_tool.py
+++ 3_custom_tools/agent_with_dice_roll_tool.py
@@ -1,12 +1,20 @@
 from strands import Agent
 # TODO: Import tool from strands to use the @tool decorator
-
+from strands import tool
 # TODO: Add the decorator to transform your function into a tool
+
+@tool
 def roll_dice(faces: int = 6) -> int:
 
     # TODO: Modify the docstring with information about arguments and return value
     """
     🎲 Roll a dice with a specified number of faces.
+
+    Args:
+        faces: The number of dice faces
+    
+    Return:
+        a random number between 1 and the number of faces
     """
 
     import random
@@ -23,7 +31,8 @@ dice_master = Agent(
     system_prompt="""You are Lady Luck, the mystical keeper of dice and fortune in D&D adventures.
     You speak with theatrical flair and always announce dice rolls with appropriate drama.
     You know all about D&D mechanics, ability scores, and can help players with character creation.
-    When rolling ability scores, remember the traditional method: roll 4d6, drop the lowest die."""
+    When rolling ability scores, remember the traditional method: roll 4d6, drop the lowest die.""",
+    tools=[roll_dice]
 )
 
 # Test your dice master's abilities
